69237cf497fcbfea0f9e99fd3d4520281c1837ee
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-28 14:58:26 -0500

    new files

--- src/main/java/com/cybertek/dto/ProjectDTO.java
+++ src/main/java/com/cybertek/dto/ProjectDTO.java
@@ -2,9 +2,15 @@ package com.cybertek.dto;
 
 
 import com.cybertek.utils.Status;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ProjectDTO {
 
     private String projectName;
