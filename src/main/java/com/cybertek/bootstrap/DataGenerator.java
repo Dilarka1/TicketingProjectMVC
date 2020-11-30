718b7403f471219a5278c26fc56458ed0e76dff3
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-30 13:33:53 -0500

    data

--- src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -2,12 +2,13 @@ package com.cybertek.bootstrap;
 
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.dto.RoleDTO;
+import com.cybertek.dto.TaskDTO;
 import com.cybertek.dto.UserDTO;
 import com.cybertek.utils.Gender;
 import com.cybertek.utils.Status;
-import com.cybertek.implementation.RoleServiceImpl;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.RoleService;
+import com.cybertek.service.TaskService;
 import com.cybertek.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -20,19 +21,21 @@ public class DataGenerator implements CommandLineRunner {
     RoleService roleService;
     UserService userService;
     ProjectService projectService;
+    TaskService taskService;
 
-    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService, TaskService taskService) {
         this.roleService = roleService;
         this.userService = userService;
         this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        RoleDTO adminRole = new RoleDTO(1L,"Admin");
-        RoleDTO managerRole = new RoleDTO(2L,"Manager");
-        RoleDTO employeeRole = new RoleDTO(3L,"Employee");
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
 
         roleService.save(adminRole);
         roleService.save(managerRole);
@@ -64,96 +67,22 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user7);
         userService.save(user8);
 
-        ProjectDTO project1 = new ProjectDTO("Spring MVC","PR001",user1, LocalDate.now(),LocalDate.now().plusDays(25),"Creating Controllers", Status.OPEN);
-        ProjectDTO project2 = new ProjectDTO("Spring ORM","PR002",user2, LocalDate.now(),LocalDate.now().plusDays(10),"Creating Database", Status.IN_PROGRESS);
-        ProjectDTO project3 = new ProjectDTO("Spring Container","PR003",user1, LocalDate.now(),LocalDate.now().plusDays(32),"Creating Container", Status.UAT_TEST);
+        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user1, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.UAT_TEST);
 
         projectService.save(project1);
         projectService.save(project2);
         projectService.save(project3);
 
+        TaskDTO task1 = new TaskDTO(1L, project1, user8, "Controller", "Request Mapping", Status.IN_PROGRESS, LocalDate.now().minusDays(4));
+        TaskDTO task2 = new TaskDTO(2L, project3, user3, "Configuration", "Database Connnection", Status.COMPLETE, LocalDate.now().minusDays(12));
+        TaskDTO task3 = new TaskDTO(3L, project3, user6, "Mapping", "One-To-Many", Status.IN_PROGRESS, LocalDate.now().minusDays(8));
+        TaskDTO task4 = new TaskDTO(4L, project2, user7, "Dependency Injection", "Autowired", Status.UAT_TEST, LocalDate.now().minusDays(20));
+
+        taskService.save(task1);
+        taskService.save(task2);
+        taskService.save(task3);
+        taskService.save(task4);
     }
 }
-
-
-
-
-
-
-
-
-//package com.cybertek.bootstrap;
-//
-//import com.cybertek.dto.ProjectDTO;
-//import com.cybertek.dto.RoleDTO;
-//import com.cybertek.dto.UserDTO;
-//import com.cybertek.implementation.RoleServiceImpl;
-//import com.cybertek.service.ProjectService;
-//import com.cybertek.service.RoleService;
-//import com.cybertek.service.UserService;
-//import com.cybertek.utils.Gender;
-//import com.cybertek.utils.Status;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//import java.time.LocalDate;
-//
-//@Component
-//public class DataGenerator implements CommandLineRunner {
-//
-//    RoleService roleService;
-//    UserService userService;
-//    ProjectService projectService;
-//
-//    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
-//        this.roleService = roleService;
-//        this.userService = userService;
-//        this.projectService = projectService;
-//    }
-//
-//    @Autowired
-//    public DataGenerator(RoleService roleService, UserService userService) {
-//        this.roleService = roleService;
-//        this.userService = userService;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//        RoleDTO adminRole = new RoleDTO(1L, "Admin");
-//        RoleDTO managerRole = new RoleDTO(2L, "Manager");
-//        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
-//
-//        roleService.save(adminRole);
-//        roleService.save(managerRole);
-//        roleService.save(employeeRole);
-//
-//        UserDTO user1 = new UserDTO("John", "Kesy",
-//                "admin@cybertek.com", "abc", true, "7459684532", adminRole, Gender.MALE);
-//        UserDTO user5 = new UserDTO("John", "Kesy",
-//                "admin2@cybertek.com", "abc", true, "7459684532", adminRole, Gender.MALE);
-//        UserDTO user2 = new UserDTO("Delisa",
-//                "Norre", "T001@cybertek.com", "123", true, "8567412358", managerRole, Gender.FEMALE);
-//        UserDTO user3 = new UserDTO("Craig", "Jark",
-//                "P001@cybertek.com", "123", true, "7777775566", employeeRole, Gender.MALE);
-//        UserDTO user4 = new UserDTO("Shaun",
-//                "Hayns", "S001@cybertek.com", "123", true, "3256987412", employeeRole, Gender.MALE);
-//
-//
-//        userService.save(user1);
-//        userService.save(user2);
-//        userService.save(user3);
-//        userService.save(user4);
-//        userService.save(user5);
-//
-//
-//        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
-//        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
-//        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user3, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.UAT_TEST);
-//
-//        projectService.save(project1);
-//        projectService.save(project2);
-//        projectService.save(project3);
-//    }
-//}
