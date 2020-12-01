267be38b319be634c14540674f581e558b7d874c
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-30 22:02:46 -0500

    last for today

--- src/main/java/com/cybertek/dto/TaskDTO.java
+++ src/main/java/com/cybertek/dto/TaskDTO.java
@@ -4,12 +4,11 @@ import com.cybertek.utils.Status;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@ToString
 public class TaskDTO {
 
     private Long id;
@@ -19,4 +18,14 @@ public class TaskDTO {
     private String taskDetail;
     private Status taskStatus;
     private LocalDate assignedDate;
+
+    public TaskDTO(ProjectDTO project, UserDTO assignedEmployee, String taskSubject, String taskDetail, Status taskStatus, LocalDate assignedDate) {
+        this.project = project;
+        this.assignedEmployee = assignedEmployee;
+        this.taskSubject = taskSubject;
+        this.taskDetail = taskDetail;
+        this.taskStatus = taskStatus;
+        this.assignedDate = assignedDate;
+        this.id = UUID.randomUUID().getMostSignificantBits();
+    }
 }
